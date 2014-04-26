c955a2fed3d3a2eef257ab45545493bcdf8a2a89
Takashi Honda <takashi.0628.honda@gmail.com>
2014-04-27 00:56:10 +0900

    change style

--- script/editme.js
+++ script/editme.js
@@ -1,26 +1,32 @@
 var editMe = function(object) {
-	// ==== put on your code... ====
-	var hour = document.querySelector('#hour');
-	var min = document.querySelector('#min');
-	var sec = document.querySelector('#sec');
+    // ==== put on your code... ====
+    var year  = document.querySelector('#year');
+    var month = document.querySelector('#month');
+    var date  = document.querySelector('#date');
+    var hour  = document.querySelector('#hour');
+    var min   = document.querySelector('#min');
+    var sec   = document.querySelector('#sec');
 
     var zeroPadding = function(num) {
-    	if (0 <= num && num < 10) {
-    		return '0' + num;
-    	}
-    	return num;
+        if (0 <= num && num < 10) {
+            return '0' + num;
+        }
+        return num;
     }
 
     var timer = function() {
-    	d = new Date();
-        hour.textContent = zeroPadding(d.getHours());
-        min.textContent = zeroPadding(d.getMinutes());
-        sec.textContent = zeroPadding(d.getSeconds());
+        var d = new Date();
+        year.textContent  = zeroPadding(d.getFullYear());
+        month.textContent = zeroPadding(d.getMonth() + 1);
+        date.textContent  = zeroPadding(d.getDate());
+        hour.textContent  = zeroPadding(d.getHours());
+        min.textContent   = zeroPadding(d.getMinutes());
+        sec.textContent   = zeroPadding(d.getSeconds());
     };
     
     timer();
     var t = setInterval(function() {
-    	timer();
+        timer();
     },1000);
-   	// =============================
+    // =============================
 };
